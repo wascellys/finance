a52ec94d10d15d40d0a893f07b5af00e6094c299
Edy <edywascellys@gmail.com>
2025-04-06 23:48:48 -0300

    changed equirements

--- core/settings.py
+++ core/settings.py
@@ -83,7 +83,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True   # necessário no Railway
+    )
 }
 
 
