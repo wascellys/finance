f68301d9ec4292f167070bd6dccb40250b9f5c3c
Edy <edywascellys@gmail.com>
2025-05-26 20:03:13 -0300

    Ajustes para receber validar saldo também.

--- core/settings.py
+++ core/settings.py
@@ -79,26 +79,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'FvUSgBjIZTqIDbqEaxpCadUNGiJOdqzb',
-#         'HOST': 'postgres.railway.internal',
-#         'PORT': '5432',
-#         'OPTIONS': {
-#             'sslmode': 'require',
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'FvUSgBjIZTqIDbqEaxpCadUNGiJOdqzb',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
+    }
+}
 
 
 # Password validation
