1f98e0773d9e7471ae8c291c72ed3ae08a289c95
Edy <edywascellys@gmail.com>
2025-05-26 22:25:44 -0300

    Ajustes para receber validar saldo também.

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
